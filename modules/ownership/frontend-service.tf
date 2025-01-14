b7636f4c8fbca5ede3ef402e2715e6b786c292f4
Mark <mark.bley@dynatrace.com>
2025-01-14 12:16:57 +0100

    update

--- modules/ownership/frontend-service.tf
+++ modules/ownership/frontend-service.tf
@@ -1,6 +1,6 @@
 
 module frontend-service-ownership {
     source = "../../templates/ownership"
-    ownership_config_name = "frontend_team"
+    ownership_config_name = "frontend_team_1"
     ownership_contact = "frontend@email.com"
 }
