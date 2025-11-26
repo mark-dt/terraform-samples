fe315039207c4678b8684aa73171aebf990b1664
Mark <mark.bley@dynatrace.com>
2025-11-26 20:09:12 +0100

    test

--- modules/checkout_service/checkout-service.tf
+++ modules/checkout_service/checkout-service.tf
@@ -2,7 +2,7 @@ module checkout-mz {
     source = "../../templates/management_zone"
     app_env = "PRD"
     app_id = "123"
-    app_name = "Checkout 2"
+    app_name = "Checkout 1"
 }
 
 module checkout-service-ownership {
