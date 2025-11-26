b5e2f8104dc5d7229f6808f8ba4f40983535cbd4
Mark <mark.bley@dynatrace.com>
2025-11-26 20:13:36 +0100

    consolidate configs

--- modules/management_zone/frontend-service.tf
+++ modules/management_zone/frontend-service.tf
@@ -2,5 +2,5 @@ module frontend-service-mz {
     source = "../../templates/management_zone"
     app_env = "PRD"
     app_id = "123"
-    app_name = "Frontend 2"
+    app_name = "frontend_service"
 }
--- modules/ownership/backend-service.tf
+++ modules/ownership/backend-service.tf
@@ -1,6 +1,6 @@
 
-module frontend-service-ownership {
+module backend-service-ownership {
     source = "../../templates/ownership"
-    ownership_config_name = "frontend_team_1"
+    ownership_config_name = "backend_team_1"
     ownership_contact = "frontend@email.com"
 }
